3160094fee43a718fce1a3c320b1b876d513afc2
mojtaba4549 <87696557+mojtaba4549@users.noreply.github.com>
2022-08-13 21:16:15 +0430

    callback

--- 1.mjs
+++ 1.mjs
@@ -1,21 +1,27 @@
-const user = {
-    "fn":"ali",
-    test : function(){
-        console.log("test1 is call!!!");
-        console.log(this.fn);
-        let thisparent = this;
-        // function test2(){
-        //     console.log("test2 is call!!!");
-        //     console.log(thisparent.fn);
+// const user = {
+//     "fn":"ali",
+//     test : function(){
+//         console.log("test1 is call!!!");
+//         console.log(this.fn);
+//         let thisparent = this;
+//         // function test2(){
+//         //     console.log("test2 is call!!!");
+//         //     console.log(thisparent.fn);
 
-        // }
-        const test2= () =>{
-            console.log("test2 is call");
-            console.log(this.fn);
-        }
-        test2();
-    }
-};
+//         // }
+//         const test2= () =>{
+//             console.log("test2 is call");
+//             console.log(this.fn);
+//         }
+//         test2();
+//     }
+// };
 
 
-user.test();
+// user.test();
+function calc(a,b){
+    const ret={};
+    ret['+']=a+b;
+    ret['-']=a-b;
+    ret['*']=a*b;
+}
