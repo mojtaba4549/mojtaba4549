3f793fb1da5d173855c0e35dc6b1e2b04ca4930a
mojtaba4549 <87696557+mojtaba4549@users.noreply.github.com>
2022-08-23 21:38:27 +0430

    501

---
+++ app1.mjs
@@ -0,0 +1,8 @@
+import * as lib from './lib.mjs';
+
+const r = lib.sum(10,20);
+console.log(lib.Price);
+lib.test();
+console.log(r);
+lib.updatePrice(100);
+console.log(lib.Price);
---
+++ lib.mjs
@@ -0,0 +1,12 @@
+export var Price = 10;
+export function sum(a,b){
+    return a+b;
+}
+
+export function test(){
+        console.log('test is called');
+ }
+
+export function updatePrice(v){
+    Price = v;
+}
