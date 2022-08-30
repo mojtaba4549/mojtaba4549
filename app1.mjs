701af0119b98f62db4123d16c8877b05aa16ddeb
mojtaba4549 <87696557+mojtaba4549@users.noreply.github.com>
2022-08-30 22:00:06 +0430

    05-05

--- app1.mjs
+++ app1.mjs
@@ -1,8 +1,46 @@
-import * as lib from './lib.mjs';
+// import * as lib from './lib.mjs';
 
-const r = lib.sum(10,20);
-console.log(lib.Price);
-lib.test();
-console.log(r);
-lib.updatePrice(100);
-console.log(lib.Price);
+// const r = lib.sum(10,20);
+// console.log(lib.Price);
+// lib.test();
+// console.log(r);
+// lib.updatePrice(100);
+// console.log(lib.Price);
+
+
+// let data = new Map();
+
+// data.set("fn","ali");
+// data.set("ln","reza");
+// console.log(data);
+// console.log(typeof data);
+
+// console.log(data.get("fn"));
+// console.log(data.get("ln"));
+
+// console.log(data.size);
+
+
+// console.log(data.keys());
+
+// data.delete('')
+// console.log(data.has("fn"zxcc
+
+// function test(status)
+// {
+//     return new Promise(function(resolve, reject)=>{
+//         setTimeout(()=>{
+//             if(status == 1){
+//                 const users = [
+//                     {"username":"ali","user_id":1},
+//                     "username":"reza","user_id":2}
+//                 ];
+//                 resolve(users);
+//             }
+//         })
+//     }
+// }
+
+
+
+ 
